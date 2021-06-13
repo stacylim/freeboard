330be178ab61337ea58e99df08afc38805309317
stacylim <82793273+stacylim@users.noreply.github.com>
2021-06-13 13:54:13 +0900

    20210613

---
+++ src/components/units/board/productregister/productregister.presenter.tsx
@@ -0,0 +1,18 @@
+<RegisterWholeWrqpper>
+  <ProductRegister>상품 등록하기</ProductRegister>
+
+  <ProductName>상품명</ProductName>
+  <ProductNameInput
+    name="productinput"
+    type="text"
+    placeholder="상품명을 작성해주세요."
+    onChange={saveProductName}
+  />
+  <Summary>한줄요약</Summary>
+  <SummaryInput
+    name="productinput"
+    type="text"
+    placeholder="상품명을 작성해주세요."
+    onChange={saveSummary}
+  />
+</RegisterWholeWrqpper>;
--- src/components/units/board/usedmarket/usedmarket.main.presenter.tsx
+++ src/components/units/board/usedmarket/usedmarket.main.presenter.tsx
@@ -1,1 +1,25 @@
+import { render } from "react-dom";
+import Carousel from "react-img-carousel";
+import React from "react";
+import {} from "./usedmarket.main.styles";
 
+export default function UsedmarketMainUI({}) {
+  return (
+    <MainWrapper>
+      <MainTitle>베스트 상품</MainTitle>
+      <UpperBoxWrapper></UpperBoxWrapper>
+
+      <MiddleWrqpper>
+        <ProductSearch></ProductSearch>
+        <Calendar></Calendar>
+        <SearchButton></SearchButton>
+      </MiddleWrqpper>
+
+      <BoardWrqpper></BoardWrqpper>
+
+      <BottomWrapper>
+        <ProductRegister>상품 등록하기</ProductRegister>
+      </BottomWrapper>
+    </MainWrapper>
+  );
+}

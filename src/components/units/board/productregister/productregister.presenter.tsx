bd75791326fb3a74bc2eb51a0509453d0fb10064
stacylim <82793273+stacylim@users.noreply.github.com>
2021-06-13 14:11:04 +0900

    20210613-3

--- src/components/units/board/productregister/productregister.presenter.tsx
+++ src/components/units/board/productregister/productregister.presenter.tsx
@@ -3,16 +3,64 @@
 
   <ProductName>상품명</ProductName>
   <ProductNameInput
-    name="productinput"
+    name="productNameinput"
     type="text"
     placeholder="상품명을 작성해주세요."
     onChange={saveProductName}
   />
   <Summary>한줄요약</Summary>
   <SummaryInput
-    name="productinput"
+    name="summaryinput"
     type="text"
     placeholder="상품명을 작성해주세요."
     onChange={saveSummary}
   />
+  <ProductDetail>상품설명</ProductDetail>
+  <SummaryTool></SummaryTool>
+  <ProductDetailInput
+    name="productdetailinput"
+    type="text"
+    placeholder="상품설명을 작성해주세요."
+    onChange={saveProductInfo}
+  />
+
+  <Price>판매 가격</Price>
+  <PriceInput
+    name="priceinput"
+    type="text"
+    placeholder="판매가격을 작성해주세요."
+    onChange={savePrice}
+  />
+
+  <Tag>태그입력</Tag>
+  <TagIput name="taginput" type="text" placeholder="#태그 #태그 #태그" />
+  <MapWrapper>
+    <LocationWrapper>
+      <Location>거래위치</Location>
+    </LocationWrapper>
+    <RightWrapper>
+      <GPS>GPS</GPS>
+      <Latitude>위도</Latitude>
+      <MapImg></MapImg>
+      <Longtitude>경도</Longtitude>
+      <Addr>주소</Addr>
+      <AddrInput1
+        name="addrInput1"
+        type="text"
+        placeholder="주소를 입력해주세요"
+      />
+
+      <AddrInput2
+        name="addrInput2"
+        type="text"
+        placeholder="상세주소를 입력해주세요"
+      />
+    </RightWrapper>
+    <PhotoInclu>사진 첨부</PhotoInclu>
+    <PhotoOptions>메인 사진 설정</PhotoOptions>
+    <radiobutton>사진1</radiobutton>
+    <radiobutton>사진2</radiobutton>
+  </MapWrapper>
+
+  <RegisterButton>등록하기</RegisterButton>
 </RegisterWholeWrqpper>;
